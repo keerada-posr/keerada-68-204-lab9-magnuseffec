160e373fe5cc14edb9cfbef51c85892fc2fc4549
keerada-posr <keerada.posr@bumail.net>
2026-03-12 10:20:04 +0700

    Add torque rotate script

--- Assets/Scripts/TorqueRotate.cs
+++ Assets/Scripts/TorqueRotate.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class AngularVelocity : MonoBehaviour
+public class TorqueRotate : MonoBehaviour
 {
-    public float angularSpeed;
+    public float torqueSpeed;
     Rigidbody rb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,14 +15,9 @@ public class AngularVelocity : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Keyboard.current.aKey.isPressed)
+        if (Keyboard.current.dKey.isPressed)
         {
-            rb.angularVelocity = new Vector3(0, angularSpeed, 0);
-        }
-
-        else
-        {
-            rb.angularVelocity = Vector3.zero;
+            rb.torqueRotate = new Vector3(0, torqueSpeed, 0);
         }
     }
 }
